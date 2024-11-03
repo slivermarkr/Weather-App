02772ea152180f38823b1af295f3f649f901d577
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-11-03 19:19:16 +0800

    added form and search box

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import "./styles/main.scss";
 import CityForecast from "./components/cityForecast";
 import getForecast from "./api/getForeCast";
 
+const form = document.querySelector("form");
+const search = document.querySelector("#search");
+
 const location = {
-  city: "Manila",
-  country: "Philippines",
+  city: "New York City",
+  country: "USA",
 };
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const [cityInp = "", countryInp = ""] = search.value.split(",");
+  // console.log(cityInp, countryInp);
+  location["city"] = cityInp.trim();
+  location["country"] = countryInp.trim();
+  // console.log(location);
+  parseWeatherInfo();
+});
 
 async function parseWeatherInfo() {
   const dataFromAPI = await getForecast(location);
   const cityWeatherInfo = new CityForecast(dataFromAPI);
+
+  console.log(cityWeatherInfo);
   // TODO: getCurrentCondition, 24hours forecast,
 }
 parseWeatherInfo();
