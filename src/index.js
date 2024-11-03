9cfdaf7f2e1a1f2166d8f1aa2c9cb3931089f101
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-11-03 18:05:06 +0800

    parsing only the necessary data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import "./styles/main.scss";
+class CityForecast {
+  constructor({ resolvedAddress, description, currentConditions, days } = {}) {
+    this.resolvedAddress = resolvedAddress;
+    this.description = description;
+    this.currentConditions = currentConditions;
+    this.days = days;
+  }
+}
+
+const location = {
+  city: "Manila",
+  country: "Philippines",
+};
+
 async function getForecast({ city, country } = {}) {
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}%2C%20${country}?unitGroup=metric&key=8AEQJ6VE8HFRDUQW3L4MYBPK5&contentType=json`,
@@ -8,10 +22,11 @@ async function getForecast({ city, country } = {}) {
     }
   );
   const result = await response.json();
-  console.log(result);
+  parseWeatherInfo(result);
+  // console.log(result);
 }
-const location = {
-  city: "Cebu City",
-  country: "Philippines",
-};
+function parseWeatherInfo(result) {
+  const cityWeatherInfo = new CityForecast(result);
+  console.log(cityWeatherInfo);
+}
 getForecast(location);
