39b12341a2d11db51128e235963d4227f854864a
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-11-03 16:18:47 +0800

    fetchin weather date from api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import generateJoke from "./generateJoke.js";
 import "./styles/main.scss";
-import laughing from "./assets/laughing.svg";
-
-const laughImg = document.querySelector("#laughImg");
-const getJokeBtn = document.querySelector("#jokeBtn");
-laughImg.src = laughing;
-
-getJokeBtn.addEventListener("click", generateJoke);
-generateJoke();
+async function getForecast({ city, country } = {}) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}%2C%20${country}?unitGroup=metric&key=8AEQJ6VE8HFRDUQW3L4MYBPK5&contentType=json`,
+    {
+      method: "GET",
+      headers: {},
+    }
+  );
+  const result = await response.json();
+  console.log(result);
+}
+const location = {
+  city: "Cebu City",
+  country: "Philippines",
+};
+getForecast(location);
