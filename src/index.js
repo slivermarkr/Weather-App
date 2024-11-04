c5d77f651ecc837c84f6d14be0cc76a6926660c8
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-11-04 22:36:18 +0800

    let user choose unit of measurement

--- src/index.js
+++ src/index.js
@@ -4,28 +4,43 @@ import getForecast from "./api/getForeCast";
 
 const form = document.querySelector("form");
 const search = document.querySelector("#search");
+const selectUnit = document.querySelector("#unitMeasure");
 
 const location = {
   city: "New York City",
   country: "USA",
 };
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const [cityInp = "", countryInp = ""] = search.value.split(",");
-  // console.log(cityInp, countryInp);
-  location["city"] = cityInp.trim();
-  location["country"] = countryInp.trim();
-  // console.log(location);
-  parseWeatherInfo();
-});
+let unitMeasurement = selectUnit.value;
 
 async function parseWeatherInfo() {
-  const dataFromAPI = await getForecast(location);
+  const dataFromAPI = await getForecast(location, unitMeasurement);
+
+  console.log(dataFromAPI);
   const cityWeatherInfo = new CityForecast(dataFromAPI);
 
   console.log(cityWeatherInfo);
   // TODO: getCurrentCondition, 24hours forecast,
 }
+
+selectUnit.addEventListener("change", () => {
+  if (search.value === "") return;
+
+  unitMeasurement = selectUnit.value;
+
+  parseWeatherInfo();
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const [cityInp = "", countryInp = ""] = search.value.split(",");
+
+  location["city"] = cityInp.trim();
+  location["country"] = countryInp.trim();
+
+  unitMeasurement = selectUnit.value;
+
+  parseWeatherInfo();
+  // form.reset();
+});
+
 parseWeatherInfo();
