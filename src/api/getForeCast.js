e58f52e2f015488d19ba433dd38b3c3e5af1dca1
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-11-04 22:40:37 +0800

    fixed the querying that give undefined description

--- src/api/getForeCast.js
+++ src/api/getForeCast.js
@@ -1,6 +1,6 @@
 export default async function getForecast({ city, country } = {}, unitGroup) {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}%2C%20${country}?unitGroup=${unitGroup}&include=days%2Chours%2Ccurrent&key=8AEQJ6VE8HFRDUQW3L4MYBPK5&contentType=json`,
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}%2C%20${country}?unitGroup=${unitGroup}&key=8AEQJ6VE8HFRDUQW3L4MYBPK5&contentType=json`,
     {
       method: "GET",
       headers: {},
