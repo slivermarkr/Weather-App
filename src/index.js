1a8dee28b06886aca8c4160f1a748b07b43966da
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-11-05 17:49:07 +0800

    improve the serach input split: includes space and comma

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const selectUnit = document.querySelector("#unitMeasure");
 const display = document.querySelector(".display");
 
 const location = {
-  city: "New York City",
-  country: "USA",
+  city: "Tokyo",
+  country: "Japan",
 };
 
 let unitMeasurement = selectUnit.value;
@@ -23,10 +23,11 @@ async function parseWeatherInfo(unitMeasurement) {
 
   const cityWeatherInfo = new CityForecast(dataFromAPI);
 
+  console.log(cityWeatherInfo);
+
   const displayContent = UI(cityWeatherInfo, unitMeasurement);
 
   display.appendChild(displayContent);
-  // TODO: getCurrentCondition, 24hours forecast,
 }
 
 parseWeatherInfo(unitMeasurement);
@@ -40,11 +41,11 @@ selectUnit.addEventListener("change", () => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const [cityInp = "", countryInp = ""] = search.value.split(",");
+  let [cityInp, countryInp] = search.value.split(/,|\s+/);
 
   location["city"] = cityInp.trim();
   location["country"] = countryInp.trim();
-
+  console.log(location);
   unitMeasurement = selectUnit.value;
 
   parseWeatherInfo(unitMeasurement);
